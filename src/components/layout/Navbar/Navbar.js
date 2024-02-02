3c7072c699ed3ae90f1988dcde5ee3a25e57d82d
Debarghya Chowdhury <debarghya2016@gmail.com>
2024-02-02 21:19:32 +0530

    committed

---
+++ src/components/layout/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavbarView from './NavbarView';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <NavbarView name="Amazon Clone" />
+            </div>
+        )
+    }
+}   
+                
+export default Navbar;
---
+++ src/components/layout/NavbarView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavbarView(props) {
+
+    return (
+        <div>
+            <h1><Navbar View {props.name}<h1/>
+        </div>
+    )
+    
+
+}
+   
+                
+export default NavbarView;
